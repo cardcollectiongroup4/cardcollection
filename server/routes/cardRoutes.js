b4cc8275f5ca97f618c2eba16214ebb1ff14ca9a
Adityo Prabowo <dithyprabowo51@gmail.com>
2021-02-05 01:27:33 +0700

    delete card route

--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -5,6 +5,7 @@ const { authentication } = require('../helpers/middleware');
 router.use(authentication);
 router.post('/', cardController.saveCard);
 router.get('/', cardController.getAllCards);
+router.delete('/', cardController.deleteCard);
 router.get('/generate', cardController.generate);
 
 module.exports = router;
